7873c34dea1fc3a14300c0cf2a2c5b3f4023db9b
emilynievesb <nemily1710@gmail.com>
2023-08-06 16:32:02 -0500

    feat: :sparkles: Inicialización rutas
    Se realizan las rutas iniciales para iniciar a crear servicios y demás

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import express from "express";
-import connection from "./utils/connect.js";
-// import { initAPIRoutes } from "./routes/routes.js";
+import { initAPIRoutes } from "./routes/routes.js";
 
 const app = express();
 app.use(express.json()); //! Middleware para leer json
-app.use("/api", async () => {
-  await connection();
-});
-
-// app.use("/api", initAPIRoutes());
+app.use("/api", initAPIRoutes());
 
 export default app;
